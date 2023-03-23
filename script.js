bb7a53db0fa07963b28f672dd7c0fec54db84779
Om Kudalkar <omkudalkar9@gmail.com>
2023-03-23 13:17:19 -0700

    chatbot complete. TODO: add web interface for input and output

--- script.js
+++ script.js
@@ -1,19 +1,36 @@
 import {config} from "dotenv"
 config()
 import { Configuration, OpenAIApi } from "openai"
-
+import readline from "readline"
 
 
 const openai = new OpenAIApi(new Configuration({
     apiKey : process.env.API_KEY
 }))
 
-openai.createChatCompletion({
-    model : "gpt-3.5-turbo",
-    // role is WHO the message is coming from. 
-    // content is the actual content of the message.
-    messages: [{role: "user", content: "Hello chatbot!"}]
-// creates a promise which then gives us a result. 
-}).then(res => {
-    console.log(res.data.choices[0].message.content)
+const userInterface = readline.createInterface({
+    input: process.stdin,
+    output : process.stdout
 })
+
+userInterface.prompt()
+ //listener
+ userInterface.on("line", async input => {
+    const response = await openai.createChatCompletion({
+        model : "gpt-3.5-turbo",
+        // role is WHO the message is coming from. 
+        // content is the actual content of the message.
+        messages: [{role: "user", content: input}]
+    // creates a promise which then gives us a result. 
+    })
+    console.log(response.data.choices[0].message.content)
+    userInterface.prompt()
+ })
+
+
+
+
+
+
+
+
